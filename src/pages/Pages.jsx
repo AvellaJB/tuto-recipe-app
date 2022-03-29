e1ec5483d8023887f050fe4ed937dc7b909724e4
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-03-29 22:05:11 +0200

    Connexion API et display results API

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,10 @@
+//Pages va contenir toutes les pages.(Home, Searched etc...)
+
+import React from "react";
+import Home from "./Home";
+
+function Pages() {
+  return <Home />;
+}
+
+export default Pages;
