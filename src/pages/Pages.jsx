ac4b51dd3d1da2eb02554cefb00a36cccd967c79
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-03-30 14:37:42 +0200

    ajout animation

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,19 +2,25 @@
 import Cuisine from "./Cuisine";
 import React from "react";
 import Home from "./Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
+
 function Pages() {
+  const location = useLocation();
+  //Avec router, il faut ajoute à Route une key et une locaton pour que exitBeforeEnter fonctionne.
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      {/* On met cuisine/:type pour que notre lien ne cherche pas que cuisine. Mais aussi autre chose en suite
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        {/* On met cuisine/:type pour que notre lien ne cherche pas que cuisine. Mais aussi autre chose en suite
       genre si on a american, thain ou japanese. */}
-      <Route path="/cuisine/:type" element={<Cuisine />} />
-      <Route path="/searched/:search" element={<Searched />} />
-      <Route path="/recipe/:name" element={<Recipe />} />
-    </Routes>
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
