ed619dc06b2a3e95eca6974fa56ab1c8ecdb47a2
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-03-30 11:48:13 +0200

    adding and styling search bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Category from "./components/Category.jsx";
 
 //On importe le browser router dans app.js parce qu'il faut qu'il englobe tous les éléments pour fonctionner.
 import { BrowserRouter } from "react-router-dom";
-
+import Search from "./components/Search.jsx";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
