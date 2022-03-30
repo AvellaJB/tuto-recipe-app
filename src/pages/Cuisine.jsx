08e6f4079c669ff7eca3c7eb36502bc5e26af100
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-03-30 11:30:54 +0200

    setup category fetching & pages

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,10 +1,61 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 //UseParams permet de récupérer le key word qu'on a dans notre URL.
 
 function Cuisine() {
-  return <div></div>;
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  //On envoie le sytle de cuisine dans la fonction get cuisine.
+  //Pour sa on utilise useParams pour récupérer les infos dans l'URL et les envoyer dynamiquement.
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+
+  return (
+    <Grid>
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img src={item.image} alt="" />
+            <h4>{item.title}</h4>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Cuisine;
