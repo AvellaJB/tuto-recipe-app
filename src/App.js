40f2d1301a1cfbaf8b98b62f2204dc6850478118
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-03-30 14:29:22 +0200

    adding logo + link homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import Pages from "./pages/Pages.jsx";
 import Category from "./components/Category.jsx";
 
 //On importe le browser router dans app.js parce qu'il faut qu'il englobe tous les éléments pour fonctionner.
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import Search from "./components/Search.jsx";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>Delicious</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,5 +22,22 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
